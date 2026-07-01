c57a94336981bd962bb543d1f025523e1c2ef770
yanglu <yanglucisco@163.com>
2026-07-01 14:50:56 +0800

    fix:修改minst，识别手写数字，单个和批量识别

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -13,7 +13,7 @@ def get_data():
 
 
 def init_network():
-    with open("sample_weight.pkl", 'rb') as f:
+    with open(os.path.dirname(os.path.abspath(__file__)) + "/sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
     return network
 
